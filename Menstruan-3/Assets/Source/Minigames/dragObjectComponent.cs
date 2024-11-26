4a2e1f7dd4a8dd43ccb4eab3138c6f45554fbcd8
Nahia <82345386+nahigles@users.noreply.github.com>
2024-11-26 15:47:43 +0100

    Drag y Drop sin bugs

--- Menstruan-3/Assets/Source/Minigames/dragObjectComponent.cs
+++ Menstruan-3/Assets/Source/Minigames/dragObjectComponent.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using UnityEngine;
 
 public class DragObjectComponent : MonoBehaviour
@@ -7,57 +8,86 @@ public class DragObjectComponent : MonoBehaviour
     private Vector3 _initialPos;
     private Vector3 _dropPos;
     bool _inDropZone;
+    bool _isDragging;
+    private SpriteRenderer renderer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+       renderer = GetComponent<SpriteRenderer>();
         _myTransform = transform;
+        _initialPos = _myTransform.position;
         _inDropZone = false;
+        _isDragging = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (_isDragging)
+        {
+            _myTransform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + _offset;
+        }
     }
 
     private void OnMouseDown()
     {
         //Debug.Log("Holaa he clicado");
-        _initialPos = _myTransform.position;
         _offset = _myTransform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
+        _isDragging = true;
     }
 
     private void OnMouseUp()
     {
-        if (_inDropZone) {
-            _myTransform.position = _dropPos;
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(_myTransform.position, renderer.bounds.size, _myTransform.eulerAngles.z);
+
+        int i = 0;
+        while (i < colliders.Length)
+        {
+        // Si es una zone donde se puede colocar
+            if (colliders[i].GetComponent<DropZoneComponent>() != null)
+            {
+                _inDropZone = true;
+                _myTransform.position = colliders[i].transform.position;
+                Debug.Log("Pos drop: " + _myTransform.position);
+            }
+            i++;
         }
-        else
+
+        if(!_inDropZone)
             _myTransform.position = _initialPos;
+
+        _inDropZone = false;
+        _isDragging = false;
+
+        //if (_inDropZone)
+        //{
+        //    _myTransform.position = _dropPos;
+        //    Debug.Log("Drop position puesta");
+        //}
+        //else
+        //    _myTransform.position = _initialPos;
+
+        //_isDragging = false;
         //Debug.Log("Holaa he dejado de clicar");
     }
 
-    private void OnMouseDrag()
-    {
-        Debug.Log("JAJA");
-        _myTransform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + _offset;
-        
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // Si es una zone donde se puede colocar
-        if (collision.GetComponent<DropZoneComponent>() != null) { 
-            _inDropZone = true;
-            _dropPos = collision.transform.position;
-            Debug.Log("Pos drop: " + _dropPos);
-        }
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    //// Si es una zone donde se puede colocar
+    //    //if (collision.GetComponent<DropZoneComponent>() != null)
+    //    //{
+    //    //    _inDropZone = true;
+    //    //    _dropPos = collision.transform.position;
+    //    //    Debug.Log("Pos drop: " + _dropPos);
+    //    //}
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        _inDropZone = false;
-    }
+
+    //}
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    //_inDropZone = false;
+    //}
 }
