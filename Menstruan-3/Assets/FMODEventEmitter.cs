89d76055724522e598a2bd6ded2ba8b12a1831b1
DavidRainder <davirive@ucm.es>
2024-12-08 17:08:49 +0100

    pasitos

--- Menstruan-3/Assets/FMODEventEmitter.cs
+++ Menstruan-3/Assets/FMODEventEmitter.cs
@@ -1,5 +1,7 @@
+using FMOD.Studio;
 using System.Collections.Generic;
 using UnityEngine;
+using static FMODEventData;
 
 public class FMODEventEmitter : MonoBehaviour
 {
@@ -27,20 +29,74 @@ public class FMODEventEmitter : MonoBehaviour
 
     Dictionary<string, List<FMOD.Studio.EventInstance>> activeEvents;
 
-    public void EmitEvent(string name)
+    private void EmitEvent(FMOD.Studio.EventInstance ev, string eventName)
     {
-        FMOD.Studio.EventInstance ev = FMODUnity.RuntimeManager.CreateInstance(
-            "event:/"+name);
         ev.start();
-        if(instance.activeEvents.ContainsKey(name))
+        if (instance.activeEvents.ContainsKey(eventName))
         {
-            instance.activeEvents[name].Add(ev);
+            instance.activeEvents[eventName].Add(ev);
         }
         else
         {
             List<FMOD.Studio.EventInstance> events = new List<FMOD.Studio.EventInstance>();
             events.Add(ev);
-            instance.activeEvents.Add(name, events);
+            instance.activeEvents.Add(eventName, events);
+        }
+    }
+
+    private FMOD.Studio.EventInstance createEventByName(string name)
+    {
+        return FMODUnity.RuntimeManager.CreateInstance(
+            "event:/" + name);
+    }
+
+    public void EmitEvent(string name)
+    {
+        EmitEvent(createEventByName(name), name);
+    }
+
+    public void EmitEvent(FMODEventData ev)
+    {
+        FMOD.Studio.EventInstance eventInstance = FMODUnity.RuntimeManager.CreateInstance(
+            "event:/"+ev.eventName);
+        foreach(FMODEventParameterData a in ev.parameters)
+        {
+            if(a.type == FMODUnity.ParameterType.Labeled)
+            {
+                eventInstance.setParameterByNameWithLabel(a.name, a.value);
+            }
+            else
+            {
+                float aFloat = float.Parse(a.value);
+                eventInstance.setParameterByName(a.name, aFloat);
+            }
+        }
+
+        EmitEvent(eventInstance, ev.eventName);
+    }
+
+    public void UpdateEventInstances(FMODEventData ev) {
+        if (!instance.activeEvents.ContainsKey(ev.eventName)) return;
+
+        List<FMOD.Studio.EventInstance> eventInstance = instance.activeEvents[ev.eventName];
+
+        foreach (var a in ev.parameters)
+        {
+            if (a.type == FMODUnity.ParameterType.Labeled)
+            {
+                foreach(var b in eventInstance)
+                {
+                    b.setParameterByNameWithLabel(a.name, a.value);
+                }
+            }
+            else
+            {
+                foreach (var b in eventInstance)
+                {
+                    float aFloat = float.Parse(a.value);
+                    b.setParameterByName(a.name, aFloat);
+                }
+            }
         }
     }
 
@@ -52,5 +108,28 @@ public class FMODEventEmitter : MonoBehaviour
         {
             ev.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
         }
-    } 
+    }
+
+    private void Update()
+    {
+        List<EventInstance> events = new List<EventInstance>();
+        foreach(var a in activeEvents)
+        {
+            for(int i = 0; i < a.Value.Count; ++i)
+            {
+                a.Value[i].getPlaybackState(out PLAYBACK_STATE state);
+                if(state == PLAYBACK_STATE.STOPPED)
+                {
+                    events.Add(a.Value[i]);
+                }
+            }
+
+            foreach(var remove in events)
+            {
+                a.Value.Remove(remove);
+            }
+
+            events.Clear();
+        }
+    }
 }
